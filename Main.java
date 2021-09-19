715c237690aec41cb2844c1a839aa04590b07dbd
mizukiokumura <nihonnpart4@yahoo.co.jp>
2021-09-19 15:09:17 +0900

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main(String[] args) {
+    Hero h1 = new Hero();
+
+    Hero h2 = new Hero("瑞樹");
+     System.out.println(h1.hp);
+     System.out.println(h1.name);
+     System.out.println(h2.hp);
+     System.out.println(h2.name);
+  }
+}
